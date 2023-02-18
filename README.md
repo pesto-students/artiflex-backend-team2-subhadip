c5fdcd55542d34df98e6f3e8edc8cbbb4180a76f
Jimish Gajjar <jimish.gajjar@gmail.com>
2023-02-18 14:56:37 +0530

    First Commit With Node Js Installation

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">Artiflex Node.js Backend Readme 👋</h1>
+
+## Installation💾   
+  
+`npm init`
---
+++ src/utils/interceptor.js
@@ -0,0 +1,21 @@
+export const mockRequest = () => {
+  const req = {};
+  req.body = jest.fn().mockReturnValue(req);
+  req.params = jest.fn().mockReturnValue(req);
+  return req;
+};
+
+export const mockResponse = () => {
+  const res = {};
+  res.send = jest.fn().mockReturnValue(res);
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+};
+// mockNext: () => jest.fn()
+
+export const mockLoggerFunction = () => {
+  return {
+    info: jest.fn().mock(),
+  };
+};
